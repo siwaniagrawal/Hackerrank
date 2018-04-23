ea944927267c180be499b301e86d1535b68e15b5
siwaniagrawal <siwaniagrawal18@gmail.com>
2018-04-23 12:30:44 +0530

    Hackerrank

---
+++ foor-ceil-rint.py
@@ -0,0 +1,5 @@
+import numpy
+a=numpy.array((input().strip().split(' ')),float)
+print(numpy.floor(a))
+print(numpy.ceil(a))
+print(numpy.rint(a))
--- transpose-flatten.py
+++ transpose-flatten.py
@@ -1,4 +1,4 @@
-import numpy
+    import numpy
 array = numpy.array([input().split() for y in range(int(input().split()[0]))], int)
 print(numpy.transpose(array))
 print(array.flatten())
